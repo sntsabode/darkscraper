508f5d2795f36497a352585fe05e5d8d9039805c
sntsabode <snts.abode@gmail.com>
2021-11-09 18:09:19 +0200

    test(index): add test suites

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,4 +3,6 @@ process.env.NODE_ENV = 'test'
 describe('darkscraper test suite', () => {
   require('./models/darklink.model.test')
   require('./requester/index.test')
+  require('./darksearch/index.test')
+  require('./core/index.test')
 })
