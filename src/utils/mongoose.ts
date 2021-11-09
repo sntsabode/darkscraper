2f51fc4bc60797b2eda5171c5fa979d7d24d9d67
sntsabode <snts.abode@gmail.com>
2021-11-09 09:33:27 +0200

    feat(app): mongodb helper functions

---
+++ src/utils/mongoose.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const mongouri = 'mongodb://localhost:27017/'
+const dbname = process.env.NODE_ENV !== 'test'
+  ? 'darkscraper'
+  : 'darkscraper_test'
+
+export async function connectMongo(): Promise<typeof mongoose> {
+  return mongoose.connect(`${mongouri}${dbname}`)
+    .catch(e => {
+      console.log(e)
+      return mongoose
+    })
+}
+
+export async function disconnectMongo(): Promise<void> {
+  return mongoose.disconnect()
+}
+
+export async function dropDatabase(): Promise<boolean> {
+  if (!mongouri.includes('localhost')) {
+    throw new Error(
+      '"Dropping database" is only allowed for local mongodb instances'
+    )
+  }
+
+  return mongoose.connection.db.dropDatabase()
+}
