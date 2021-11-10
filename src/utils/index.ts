41b308a2090c523e1c2a7e2d057a52d0ef309c12
sntsabode <snts.abode@gmail.com>
2021-11-10 18:01:51 +0200

    feat(utils): _

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,26 @@
+import ErrorStack from './errorstack'
 import Logger from './logger'
+
+export function waitFactory<T extends Function>(
+  callback: T,
+  howLong: number
+): () => Promise<T> {
+  Logger.debug<any>('Wait function called', callback, howLong)
+  return () => new Promise((resolve) => {
+    setTimeout(() => resolve(callback), howLong)
+  })
+}
+
+export class HandleError {
+  protected handleError<E extends { message?: string }>(
+    ...e: E[]
+  ): void {
+    Logger.error(...e)
+    ErrorStack.instance.addError()
+  }
+}
+
+export type Maybe<T> = T | undefined
 
 export function panic<E>(exitCode: number, ...e: E[]): never {
   Logger.error(...e)
