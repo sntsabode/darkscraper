0cbe5cfe3ae39a37e77be20ef0e04b676062436f
sntsabode <snts.abode@gmail.com>
2021-11-13 18:25:14 +0200

    test(darklink.model): test `updateDarkLinkPath` function

--- tests/models/darklink.model.test.ts
+++ tests/models/darklink.model.test.ts
@@ -48,11 +48,11 @@ describe('darklink model test suite', () => {
     const domain = 'http://doubletuoxp6ok2lgxvfrukbuo4gon3eb76tonsoa2kdo7njcb7xk7ad.onion'
 
     const darkLinks = await darkLinkModel.fetchDarkLinks()
-    assert(darkLinks.length === 1)
+    assert(darkLinks.length === 1, 'Dark links length error')
 
     const [darkLink] = darkLinks
-    assert(darkLink!.domain === domain)
-    assert(darkLink!.paths.length === 2)
+    assert(darkLink!.domain === domain, 'Domain error')
+    assert(darkLink!.paths.length === 2, 'Paths length error')
 
     const [path1, path2] = darkLink!.paths
 
@@ -61,6 +61,50 @@ describe('darklink model test suite', () => {
 
     assert(path2!.path === '/path_2', 'Path 2 path error')
     assert(path2!.title === 'Demo Dark Link 2', 'Path 2 title error')
+  })
+
+  it('Should call the updateDarkLinkPath function with an existing domain and path', async () => {
+    const domain = 'http://doubletuoxp6ok2lgxvfrukbuo4gon3eb76tonsoa2kdo7njcb7xk7ad.onion'
+    const path = '/'
+
+    let darkLink = await darkLinkModel.fetchDarkLink(domain)
+
+    let [path1] = darkLink!.paths
+
+    assert(path1!.blacklisted === undefined)
+
+    await darkLinkModel.updateDarkLinkPath(
+      { domain, path },
+      'blacklisted',
+      true
+    )
+
+    darkLink = await darkLinkModel.fetchDarkLink(domain)
+
+    path1 = darkLink!.paths[0]!
+
+    assert(path1.blacklisted, 'Should be blacklisted')
+  })
+
+  it('Should call the updateDarkLinkPath function with an existing domain but nonexistent path', async () => {
+    const domain = 'http://doubletuoxp6ok2lgxvfrukbuo4gon3eb76tonsoa2kdo7njcb7xk7ad.onion'
+    const path = '/dont_exist'
+
+    await darkLinkModel.updateDarkLinkPath(
+      { domain, path },
+      'crawled',
+      true
+    )
+  })
+
+  it('Should call the updateDarkLinkPath function with a nonexistent domain and nonexistent path', async () => {
+    const domain = 'http://i_dont_exist.onion'
+    const path = '/dont_exist'
+
+    await darkLinkModel.updateDarkLinkPath({ domain, path }, 'crawled', true)
+
+    const res = await darkLinkModel.fetchDarkLinks()
+    console.log(res)
   })
 
   after(async () => mongoUtils.dropDatabase()
