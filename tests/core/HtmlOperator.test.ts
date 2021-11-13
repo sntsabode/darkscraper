cb15fcc4d513e75b032c2b7c76721ba1f9082ba1
sntsabode <snts.abode@gmail.com>
2021-11-13 12:48:50 +0200

    test(app): `HtmlOperator` test suite

---
+++ tests/core/HtmlOperator.test.ts
@@ -0,0 +1,44 @@
+/*
+yarn run mocha -r ts-node/register tests/core/HtmlOperator.test.ts --timeout 900000
+*/
+
+import { assert } from 'chai'
+import HtmlOperator from '../../src/core/HtmlOperator'
+
+const htmlLinks = [
+  'http://nowhere.com/hello',
+  'http://nowhere.com/hello-world',
+  'http://nowhere.com/bye',
+  'http://nowhere.com/bye-world'
+]
+
+const html = `
+<!DOCTYPE html>
+  <body>
+    <div>
+      <a href="${htmlLinks[0]}"><p>Hello</p></a>
+      <a href="${htmlLinks[1]}"><p>World</p></a>
+    </div>
+    <footer>
+      <a href="${htmlLinks[2]}"><p>Bye</p></a>
+      <a href="${htmlLinks[3]}"><p>World</p></a>
+    </footer>
+  </body>
+</html>
+`
+
+describe('HtmlOperator test suite', () => {
+  it('Should call the getAllLinks method', (done) => {
+    const $ = HtmlOperator.parseHtml(html)
+
+    const links = HtmlOperator.getAllLinks($)
+
+    assert(links.length === 4, 'Links are not enough')
+    assert(links[0] === htmlLinks[0], 'Link error')
+    assert(links[1] === htmlLinks[1], 'Link error')
+    assert(links[2] === htmlLinks[2], 'Link error')
+    assert(links[3] === htmlLinks[3], 'Link error')
+
+    done()
+  })
+})
