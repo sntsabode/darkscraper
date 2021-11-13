13dca7133cd49739b08a1af3cf5fece4655131af
sntsabode <snts.abode@gmail.com>
2021-11-13 18:22:29 +0200

    fix(`DarkSearch`): remove `IDarkSearchResponseTuple` type

--- src/models/darklink.model.ts
+++ src/models/darklink.model.ts
@@ -1,9 +1,12 @@
 import { model, Schema } from 'mongoose'
+import { getDomainAndPath, Maybe } from '../utils'
+import Logger from '../utils/logger'
 
 export interface IDarkLinkPath {
 	path: string
 	title: string
 	blacklisted?: boolean
+  crawled?: boolean
 }
 
 export interface IDarkLink {
@@ -27,11 +30,15 @@ const DarkLinkSchema = new Schema<IDarkLink>({
 
       title: {
         type: String,
-        required: false,
         default: 'darkscraper_darklink'
       },
 
       blacklisted: {
+        type: Boolean,
+        required: false
+      },
+
+      crawled: {
         type: Boolean,
         required: false
       }
@@ -51,9 +58,73 @@ export interface ISaveDarkLinkResult {
   newPath?: boolean
 }
 
-export async function saveDarkLink(
+export async function updateDarkLinkPath<T extends keyof IDarkLinkPath>(
+  { domain, path }: { domain: string, path: string },
+  updateKey: T,
+  update: IDarkLinkPath[T]
+) {
+  const key = `paths.$.${updateKey}`
+
+  let res = await DarkLinkModel.updateOne({
+    domain, 'paths.path': path
+  }, {
+    $set: { [key]: update }
+  })
+
+  if (res.modifiedCount > 0) {
+    Logger.debug(
+      `Updated existing domain and path:\n`,
+      `Domain: ${domain}`,
+      `| Path: ${path}`,
+      `| Update: { ${updateKey}: ${update} }`
+    )
+
+    return
+  }
+
+  res = await DarkLinkModel.updateOne(
+    { domain, 'paths.path': { '$ne': path } },
+    { $push: { paths: { path } } },
+  )
+
+  if (res.modifiedCount > 0) {
+    Logger.debug(
+      `Updated existing domain new path:\n`,
+      `Domain: ${domain}`,
+      `| Path: ${path}`,
+      `| Update: { ${updateKey}: ${update} }`
+    )
+
+    return
+  }
+
+  await saveDarkLink(domain, { path })
+
+  Logger.debug(
+    `Added new domain and new path:\n`,
+    `Domain: ${domain}`,
+    `| Path: ${path}`,
+    `| Update: { ${updateKey}: ${update} }`
+  )
+}
+
+export async function saveDarkLink (
+  linkOrDomain: string,
+  path?: Partial<IDarkLinkPath>
+): Promise<ISaveDarkLinkResult> {
+  if (path) { return saveDarkLinkMain(linkOrDomain, path) }
+
+  const { domain, path: pathName } = getDomainAndPath(linkOrDomain)
+
+  return saveDarkLinkMain(domain, {
+    path: pathName,
+    title: 'Infiltrator result'
+  })
+}
+
+async function saveDarkLinkMain(
   domain: string,
-  path: IDarkLinkPath
+  path: Partial<IDarkLinkPath>
 ): Promise<ISaveDarkLinkResult> {
   const darkLink = new DarkLinkModel({
     domain,
@@ -83,6 +154,9 @@ export async function saveDarkLink(
   )
 }
 
+export async function fetchDarkLink(domain: string): Promise<Maybe<IDarkLinkModel>> {
+  return DarkLinkModel.findOne({ domain })
+}
 
 export async function fetchDarkLinks(limit = 1000): Promise<IDarkLinkModel[]> {
   return DarkLinkModel.find({ }).limit(limit)
