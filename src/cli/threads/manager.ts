96665901219c86a4f1ebd7aa1ffd98af14c92260
sntsabode <snts.abode@gmail.com>
2021-11-14 18:56:22 +0200

    fix(manager): remove redundant worker module

--- src/cli/threads/manager.ts
+++ src/cli/threads/manager.ts
@@ -1,9 +1,7 @@
 import { Worker } from 'worker_threads'
 
-export async function bootThread(path: string): Promise<number> {
-  const worker = new Worker('./dist/cli/threads/worker', {
-    workerData: { path }
-  })
+export async function bootThread<T>(path: string, workerData: T) {
+  const worker = new Worker(path, { workerData })
 
   return new Promise((resolve) => {
     worker.on('exit', (exitcode) => resolve(exitcode))
--- src/cli/threads/worker.ts
+++
@@ -1,5 +0,0 @@
-import { resolve } from 'path'
-import { workerData } from 'worker_threads'
-
-//require('ts-node').register()
-require(resolve(__dirname, workerData.path))
