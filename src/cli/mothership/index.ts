4a63c54a16d166889982539df903840404f4bd1f
sntsabode <snts.abode@gmail.com>
2021-11-14 18:55:48 +0200

    feat(mothership): began `fetchCoreConfiguration`

--- src/cli/mothership/index.ts
+++ src/cli/mothership/index.ts
@@ -1,21 +1,27 @@
+import { readFileSync, writeFileSync } from 'fs'
 import argv from '../argv'
 import { bootThread } from '../threads/manager'
 import crawler from '../../lib'
+import Logger from '../../lib/utils/logger'
+import colors from '../../lib/utils/colors'
+import { connectMongo, disconnectMongo, dropDatabase } from '../../lib/utils/mongoose'
 
-export default async function main(argv: argv) {
+export default async function mothership(argv: argv) {
   if (argv.purge) {
     await purgeDatabase()
   }
 
-  if (argv.crawler && argv.server) {
-    const processes: (() => Promise<number>)[] = []
+  let coreConfiguration = fetchCoreConfiguration()
 
-    const pathToCrawlerEntryPoint = '../mothership/crawler.worker'
-    processes.push(() => bootThread(pathToCrawlerEntryPoint))
+  if (argv.crawler && argv.server) {
+    const processes: (() => Promise<any>)[] = []
+
+    const pathToCrawlerEntryPoint = './dist/cli/mothership/crawler.worker'
+    processes.push(() => bootThread(pathToCrawlerEntryPoint, coreConfiguration))
 
     return Promise.all(processes.map(process => process()))
   } else if (argv.crawler) {
-    return crawler()
+    return crawler(coreConfiguration)
   } else if (argv.server) {
     return
   }
@@ -23,6 +29,41 @@ export default async function main(argv: argv) {
   return
 }
 
-async function purgeDatabase() {
+function fetchCoreConfiguration() {
+  const configPath = '/usr/darkscraper/core_config.json'
 
+  try {
+    return JSON.parse(readFileSync(configPath).toString())
+  } catch (e: any) {
+    if (e.code !== 'ENOENT') {
+      throw e
+    }
+
+    writeFileSync(configPath, JSON.stringify({}))
+  }
+}
+
+async function purgeDatabase() {
+  await connectMongo()
+
+  const databaseDropped = await dropDatabase()
+  if (!databaseDropped) {
+    Logger.error('Failed to purge database.')
+    console.log(
+      'If you really wish to purge the database...\n',
+      'Open a mongo shell by entering', ...colors.cyan('mongo'), 'in a terminal.\n',
+      'Once the mongo repl is open enter the following commands in sequence to purge the darkscraper database.\n',
+      '>', ...colors.cyan('show dbs\n'),
+      '>', ...colors.cyan('use darkscraper\n'),
+      '>', ...colors.cyan('db.dropDatabase()\n'),
+      'If no error was thrown, the database was successfully purged.\n',
+      'Run...\n',
+      '>', ...colors.cyan('show dbs\n'),
+      'again to verify the database was purged.\n'
+    )
+  } else {
+    Logger.success('Successfully purged your local dark link database.')
+  }
+
+  return disconnectMongo()
 }
