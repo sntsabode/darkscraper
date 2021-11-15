13683cd19d1545123be2105ab51a4f5bdd7328db
sntsabode <snts.abode@gmail.com>
2021-11-15 17:49:09 +0200

    feat(lib): `Reconnaissance` history logic

--- src/lib/core/Reconnaissance.ts
+++ src/lib/core/Reconnaissance.ts
@@ -1,6 +1,9 @@
-import DarkSearch from '../darksearch'
+import { mkdirSync, readFileSync, writeFileSync } from 'fs'
+import DarkSearch, { IDarkSearchHistory } from '../darksearch'
+import { configDir, darkSearchHistoryPath } from '../dirs'
 import { ISaveDarkLinkResult, saveDarkLink } from '../models/darklink.model'
 import { Maybe, waitFactory } from '../utils'
+import colors from '../utils/colors'
 import Logger from '../utils/logger'
 
 export interface IBaseQueries {
@@ -15,17 +18,26 @@ export default class Reconnaissance {
     baseQueries: IBaseQueries,
     throttle: number = 10000
   ) {
-    for (const [query, page] of Object.entries(baseQueries)) {
+    for (const [query, page] of Object.entries(
+      this.#contructBaseQueries(baseQueries)
+    )) {
       this.#DarkSearches.push(new DarkSearch(query, page))
+      Logger.info(`DarkSearch instance for ${query}, `
+        + `starting at page ${page}`
+      )
     }
 
     this.#throttle = throttle
   }
 
-  #WORK = true
+  #i = 1
+  #work = true
   runReconnaissance = async () => {
-    while(this.#WORK) {
+    while(this.#work) {
       await this.work()
+
+      Logger.info(`Reconnaissance interation ${this.#i} complete.`)
+      this.#i++
     }
   }
 
@@ -42,13 +54,38 @@ export default class Reconnaissance {
     const res = await this.runDarkSearches()
 
     const saveResults = await this.saveDarkSearchLinks(res)
-    Logger.debug(saveResults)
+    this.saveDarkSearchHistory()
+    Logger.verbose(saveResults)
   }
 
   runDarkSearches = async (): Promise<Maybe<string[]>[]> =>
     Promise.all(this.#DarkSearches.map(
       ds => ds.search()
     ))
+
+  saveDarkSearchHistory = () => {
+    const history = this.fetchDarkSearchHistory()
+
+    for (const ds of this.#DarkSearches) {
+      history[ds.getQuery] = ds.getPage
+    }
+
+    try {
+      writeFileSync(darkSearchHistoryPath, JSON.stringify(history))
+    } catch (e: any) {
+      this.#handleENOENTerror(e, history)
+    }
+  }
+
+  fetchDarkSearchHistory = (): IDarkSearchHistory => {
+    try {
+      return JSON.parse(
+        readFileSync(darkSearchHistoryPath).toString()
+      )
+    } catch (e: any) {
+      return this.#handleENOENTerror(e)
+    }
+  }
 
   saveDarkSearchLinks = async (
     res: Maybe<string[]>[]
@@ -59,4 +96,46 @@ export default class Reconnaissance {
         : res
     )
   )
+
+  #contructBaseQueries = (baseQueries: IBaseQueries): IBaseQueries => {
+    const history = this.fetchDarkSearchHistory()
+
+    for (const [query, page] of Object.entries(baseQueries)) {
+      if (history.hasOwnProperty(query)) {
+        if (history[query]! > page) {
+          Logger.error(`Entered page ${page} for `
+            + `${colors.cyan(query)[0]} query, but history `
+            + `is on page ${history[query]}. Giving precedence to history.`
+          )
+
+          continue
+        } else {
+          history[query] = page
+          continue
+        }
+      }
+
+      history[query] = page
+    }
+
+    return history
+  }
+
+  #handleENOENTerror = <T extends { code: string }>(
+    e: T,
+    history: IDarkSearchHistory = { }
+  ): IDarkSearchHistory => {
+    if (e.code !== 'ENOENT') { throw e }
+
+    try {
+      writeFileSync(darkSearchHistoryPath, JSON.stringify(history))
+      return { }
+    } catch (e: any) {
+      if (e.code !== 'ENOENT') { throw e }
+
+      mkdirSync(configDir)
+      writeFileSync(darkSearchHistoryPath, JSON.stringify(history))
+      return { }
+    }
+  }
 }
