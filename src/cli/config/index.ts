cc1443ee2d35558dac32d6aaf21d4ac0b9c7654d
sntsabode <snts.abode@gmail.com>
2021-11-15 03:02:29 +0200

    feat(cli): add log option to configure menu

--- src/cli/config/index.ts
+++ src/cli/config/index.ts
@@ -5,10 +5,16 @@ import { ICoreConfiguration } from '../../lib/core'
 import { IBaseQueries } from '../../lib/core/Reconnaissance'
 import colors from '../../lib/utils/colors'
 import Logger from '../../lib/utils/logger'
-import { fetchCoreConfigFile, validateCoreConfig } from './core.config'
+import { fetchCoreConfigFile, saveCoreConfigFile, validateCoreConfig } from './core.config'
 
 export const configDir = `${require('os').homedir()}/darkscraper`
 export const coreConfigPath = `${configDir}/core_config.json`
+
+export type IConfigOption =
+  | 'purge'
+  | 'crawler'
+  | 'log'
+  | 'exit'
 
 export default async function configureMenu() {
   logIntro()
@@ -18,34 +24,35 @@ export default async function configureMenu() {
   while (work) {
     const option = await ask('darkscraper::configure>') as IConfigOption
     if (option === 'purge') {
-      await purge()
+      await purgeOption()
     } else if (option === 'crawler') {
-
+      await crawlerOption()
+    } else if (option === 'log') {
+      await logOption()
     } else if (option === 'exit') {
       work = false
     }
   }
 }
 
-async function purge() {
+async function purgeOption() {
   Logger.info(...colors.red('purge'), 'selected. You can run the following commands:')
   console.log(tab, ...colors.red('crawler'), tab, '-- Delete your', ...colors.cyan('crawler configuration'))
   console.log(tab, ...colors.red('server'), tab, '-- Delete your', ...colors.cyan('server configuration'))
   console.log(tab, ...colors.green('exit'), tab, '-- Exit the purge input loop')
 
+  const log = `darkscraper::configure::${colors.red('purge')[0]}>`
   let work = true
 
   while (work) {
-    const option = await ask('darkscraper::configure::purge>')
+    const option = await ask(log)
     if (option === 'crawler') {
-      const success = deleteAndLogENOENT(coreConfigPath, option)
-      if (success) {
-        Logger.success('Successfully deleted your crawler configuration file.')
-      }
-      work = false
-    } else if (option === 'exit') {
+      await purgeOptionWorker(coreConfigPath, option)
       work = false
     } else if (option === 'server') {
+      await purgeOptionWorker('', option)
+      work = false
+    } else if (option === 'exit') {
       work = false
     } else {
       Logger.error('Entered an invalid option')
@@ -54,24 +61,86 @@ async function purge() {
   }
 }
 
-function deleteAndLogENOENT(path: string, which: string): boolean {
-  try {
-    unlinkSync(path)
+async function purgeOptionWorker(pathToConfigFile: string, option: string) {
+  const confirmation = await confirmPrompt(`Are you sure you want to purge your ${colors.red(option)[0]} configuration`)
+  if (!confirmation) { return }
 
-    return true
-  } catch (e: any) {
-    if (e.code !== 'ENOENT') { throw e }
+  deleteAndLogENOENT(pathToConfigFile, option)
+}
 
-    Logger.error(`Could not find the ${colors.red(which)[0]} configuration file.`)
+async function confirmPrompt(msg: string) {
+  const { confirm } = await inquirer.prompt<{ confirm: boolean }>([
+    {
+      name: 'confirm',
+      type: 'confirm',
+      message: msg
+    }
+  ]).catch(e => {console.log(e); return { confirm: true }})
 
-    return false
+  return confirm
+}
+
+async function crawlerOption() {
+  const config = await askForCoreConfig()
+  saveCoreConfigFile(config)
+
+  Logger.success('Successfully saved your configuration.')
+}
+
+async function logOption() {
+  Logger.info(...colors.cyan('log'), 'selected. You can run the following commands:')
+  console.log(tab, 'crawler', tab, '-- Log your', ...colors.cyan('crawler configuration'))
+  console.log(tab, 'server', tab, '-- Loge your', ...colors.cyan('server configuration'))
+  console.log(tab, ...colors.green('exit'), tab, '-- Exit the log input loop')
+
+  let work = true
+
+  while (work) {
+    const option = await ask('darkscraper::configure::log>')
+    if (option === 'crawler') {
+      logCoreConfigFile()
+      work = false
+    } else if (option === 'server') {
+      work = false
+    } else if (option === 'exit') {
+      work = false
+    } else {
+      Logger.error('Entered an invalid option')
+      Logger.error('Please enter', ...colors.green('crawler'), 'or', ...colors.green('server'))
+    }
   }
 }
 
-export type IConfigOption =
-  | 'purge'
-  | 'crawler'
-  | 'exit'
+function logCoreConfigFile() {
+  const coreConfig = fetchCoreConfigFile()
+
+  Logger.info('Crawler configuration:')
+
+  console.log(tab, 'Panic trigger:', ...colors.white(coreConfig?.panicTrigger))
+  console.log(tab, 'Recon throttle:', ...colors.white(coreConfig?.reconThrottle))
+  console.log(tab, 'Infil throttle:', ...colors.white(coreConfig?.infilThrottle))
+  console.log(tab, 'Infil wait before run time:', ...colors.white(coreConfig?.infilWaitBeforeRunTime))
+  if (coreConfig?.baseQueries) {
+    console.log(tab, 'Base queries:')
+    for (const [query, page] of Object.entries(coreConfig?.baseQueries)) {
+      console.log(`${tab}${tab}`, 'Query:', ...colors.cyan(query))
+      console.log(`${tab}${tab}`, 'Page:', page, '\n')
+    }
+  } else {
+    console.log(tab, 'Base queries:', undefined, '\n')
+  }
+}
+
+function deleteAndLogENOENT(path: string, which: string) {
+  try {
+    unlinkSync(path)
+    Logger.success('Successfully deleted your crawler configuration file.')
+  } catch (e: any) {
+    if (e.code !== 'ENOENT') { throw e }
+
+    Logger.error(`Could not find a ${colors.red(which)[0]} configuration file.`)
+  }
+}
 
 export async function fetchOrAskForCoreConfig() {
   const config = fetchCoreConfigFile()
@@ -158,11 +227,11 @@ export async function ask(log: string): Promise<string> {
 const tab = '    '
 
 function logIntro() {
-  Logger.info('You have entered the', ...colors.cyan('configure menu'))
-  Logger.info('You can run the following commands: ')
+  Logger.info('You have entered the', ...colors.cyan('configure menu'), 'You can run the following commands:')
   console.log(`${tab}${colors.red('purge')[0]} ${tab} -- Delete configuration files`)
-  console.log(`${tab}${colors.magenta('crawler')[0]} ${tab} -- Configure your crawler instance`)
-  console.log(`${tab}${colors.red('exit')[0]} ${tab} -- Exit the configure menu`)
+  console.log(`${tab}crawler ${tab} -- Configure your crawler instance`)
+  console.log(`${tab}log ${tab} -- Log your configuration files`)
+  console.log(`${tab}${colors.green('exit')[0]} ${tab} -- Exit the configure menu`)
 
   console.log()
 }
