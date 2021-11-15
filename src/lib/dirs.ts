d147f28fa89e42715239f6ce634b5bbfa9f1f313
sntsabode <snts.abode@gmail.com>
2021-11-15 17:47:24 +0200

    feat(lib): dirs module

---
+++ src/lib/dirs.ts
@@ -0,0 +1,3 @@
+export const configDir = `${require('os').homedir()}/darkscraper`
+export const coreConfigPath = `${configDir}/core_config.json`
+export const darkSearchHistoryPath = `${configDir}/darksearch_history.json`
