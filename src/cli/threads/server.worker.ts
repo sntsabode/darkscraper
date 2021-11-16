33d38016ad7b06439af2201913218bd317ac7f5c
sntsabode <snts.abode@gmail.com>
2021-11-16 14:47:46 +0200

    fix(cli): set loglevel in worker module

--- src/cli/threads/server.worker.ts
+++ src/cli/threads/server.worker.ts
@@ -1,7 +1,9 @@
-import { parentPort } from 'worker_threads'
+import { parentPort, workerData } from 'worker_threads'
 import server from '../../api/server'
 import colors from '../../lib/utils/colors'
 import Logger from '../../lib/utils/logger'
+
+Logger.logLevel = workerData.logLevel
 
 const tag = colors.yellow('[worker]')[0]
 
