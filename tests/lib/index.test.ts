cf6825298a8e46a4cdc0ab951c56cfa7af2c0559
sntsabode <snts.abode@gmail.com>
2021-11-16 14:50:27 +0200

    test(lib): lib test suite

---
+++ tests/lib/index.test.ts
@@ -0,0 +1,8 @@
+describe('lib test suite', () => {
+  require('./models/darklink.model.test')
+  require('./requester/index.test')
+  require('./darksearch/index.test')
+  require('./core/Reconnaissance.test')
+  require('./core/HtmlOperator.test')
+  require('./core/Infiltrator.test')
+})
