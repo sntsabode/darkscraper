fcbd0c47648cb1c0f4d2abd51a6d9d4ca98c6262
sntsabode <snts.abode@gmail.com>
2021-11-16 14:50:50 +0200

    fix(test): fix imports

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,6 @@
 process.env.NODE_ENV = 'test'
 
 describe('darkscraper test suite', () => {
-  require('./lib/models/darklink.model.test')
-  require('./lib/requester/index.test')
-  require('./lib/darksearch/index.test')
-  require('./lib/core/Reconnaissance.test')
-  require('./lib/core/HtmlOperator.test')
-  require('./lib/core/Infiltrator.test')
+  require('./lib/index.test')
+  require('./api/index.test')
 })
