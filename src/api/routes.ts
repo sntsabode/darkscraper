0e6384bf934147cd457f72285587e2d00694557a
sntsabode <snts.abode@gmail.com>
2021-11-18 22:37:56 +0200

    feat(routes): `/search-dark-links`

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -4,5 +4,6 @@ import * as MainController from './controllers/main.controller'
 const router = Router()
 
 router.get('/fetch-dark-links', MainController.fetchDarkLinks)
+router.get('/search-dark-links', MainController.searchDarkLinks)
 
 export { router }
