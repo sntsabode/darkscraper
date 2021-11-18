0bbef07a4cd4bda608987bce43e4c5374d29c0ac
sntsabode <snts.abode@gmail.com>
2021-11-18 22:38:21 +0200

    feat(main.controller): `searchDarkLinks`

--- src/api/controllers/main.controller.ts
+++ src/api/controllers/main.controller.ts
@@ -1,11 +1,56 @@
 import { Request, Response } from 'express'
-import { fetchDarkLinks as fetchDarkLinksFromDB } from '../../lib/models/darklink.model'
+import {
+  fetchDarkLinks as fetchDarkLinksFromDB,
+  searchDarkLinks as searchDarkLinksInDB
+} from '../../lib/models/darklink.model'
 import Logger from '../../lib/utils/logger'
+
+interface ISearchDarkLinksQuery {
+  search: string
+  skip?: string
+  limit?: string
+}
+
+export async function searchDarkLinks(
+  req: Request,
+  res: Response
+): Promise<Response> {
+  if (!req.query.search) {
+    return res.status(400).send({
+      links: null,
+      error: 'Please provide a search'
+    })
+  }
+
+  const query
+    = req.query as unknown as ISearchDarkLinksQuery
+
+  try {
+    return res.status(200).send({
+      links: await searchDarkLinksInDB(
+        query.search,
+        !!query.skip
+          ? parseInt(query.skip)
+          : 0,
+        !!query.limit
+          ? parseInt(query.limit)
+          : 25
+      )
+    })
+  } catch (e: any) {
+    console.log(e)
+
+    const error = e.message
+      ?? 'An unexpected error occurred searching for the dark links.'
+
+    return res.status(500).send({ link: null, error })
+  }
+}
 
 export async function fetchDarkLinks(
   req: Request,
   res: Response
-) {
+): Promise<Response> {
   try {
     return res.status(200).send({
       links: await fetchDarkLinksFromDB(
